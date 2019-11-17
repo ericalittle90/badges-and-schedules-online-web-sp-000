5832cdd384549645959b6025dfeaab620807d524
Erica Little <eteney90@gmail.com>
2019-11-17 16:38:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,12 @@ def assign_rooms(attendees)
   "Hello, #{name}! You'll be assigned to room #{index+1}!"
 end
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
