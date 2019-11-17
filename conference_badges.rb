60a5aca3b2685d9db593ab779a3ced823d84c712
Erica Little <eteney90@gmail.com>
2019-11-17 16:31:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,7 @@ end
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index { |name, index|
-1-7 = index
-  }
+  attendees.each_with_index do |name, index|
+  "Hello, #{name}! You'll be assigned to room #{index+1}!"
+end
+end
