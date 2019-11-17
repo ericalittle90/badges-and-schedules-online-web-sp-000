66bf95efba568efc6b36fb4f9c5e5676377b9cee
Erica Little <eteney90@gmail.com>
2019-11-17 16:06:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,6 @@ def badge_maker(name)
 return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
-names = []
-attendees.each do |names|
-end
-badge_maker(name)
+def batch_badge_creator(attendees)
+return attendees.collect {|name| "Hello, my name is #{name}."}
 end
